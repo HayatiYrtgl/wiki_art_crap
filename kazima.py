afdfd17acc6539b5ebe026240afc98f7d02475d9
HayatiYrtgl <hayatiyurtoglu71@gmail.com>
2024-04-29 02:22:01 +0300

    first

--- kazima.py
+++ kazima.py
@@ -1,104 +1,66 @@
-import urllib.request
-from bs4 import BeautifulSoup
-import requests
 import os
+import aiohttp
+import asyncio
+from bs4 import BeautifulSoup
 
-
-# class for scrap data from wikiart
-class ScrapData:
-
-    """This class scrap pictures from urls"""
+class ScrapDataAsync:
     def __init__(self):
-
-        # initializer
         self.painters_names = ["salvador-dali", "leonardo-da-vinci", "vincent-van-gogh",
                                "claude-monet", "pablo-picasso", "sandro-botticelli",
                                "henri-matisse", "raphael", "frida-kahlo",
                                "paul-cezanne", "michelangelo"]
-        self.current_painter = None
         self.base_url = "https://www.wikiart.org/en/{}/all-works/text-list"
         self.main_url = "https://www.wikiart.org"
         self.all_painting_links = []
 
+    async def get_content(self, session, url):
+        async with session.get(url) as response:
+            return await response.text()
 
-    # get request to page
-    def get_requests(self, painter: str, url: str):
-
-        """this method request the url and returns the content of the page"""
+    async def get_requests(self, painter, url, session):
         self.current_painter = painter
+        content = await self.get_content(session, url)
+        return content
 
-        # requests
-        request = requests.get(url)
-
-        # status code control
-        if request.status_code != 200:
-            print(request.status_code)
-            return None
-
-        print("status code approved")
-        return request.content
-
-    # scrap the all lists href
-    def scrap_hrefs(self, content):
-        """This method returns the hrefs of page"""
+    async def scrap_hrefs(self, content):
         soup = BeautifulSoup(content, "lxml")
-
         all_hrefs = soup.select(".painting-list-text-row a")
-        href_list = []
-
-        for href in all_hrefs:
-            href_list.append(self.main_url+href["href"])
+        href_list = [self.main_url + href["href"] for href in all_hrefs]
         return href_list
 
-    # scrape images
-    @staticmethod
-    def scrap_image_href(content):
-        """this method scraps iamge href"""
+    async def scrap_image_href(self, content):
         soup = BeautifulSoup(content, "lxml")
         image = soup.select_one(".btn-overlay-wrapper-artwork img")
         return image["src"]
 
-    # download image
-    def download_image(self, url: str, painter: str, file_name: int):
-        # control
-        self.control_directory(painter)
-        opener = urllib.request.build_opener()
-        opener.addheaders = [('User-agent', 'Mozilla/5.0')]
-        urllib.request.install_opener(opener)
-        urllib.request.urlretrieve(url, f"paintings/{painter}/{file_name}.png")
+    async def download_image(self, url, painter, file_name, session):
+        if not os.path.exists(f"paintings/{painter}"):
+            os.makedirs(f"paintings/{painter}")
+
+        async with session.get(url) as response:
+            with open(f"paintings/{painter}/{file_name}.png", "wb") as f:
+                f.write(await response.read())
+
+    async def main_scraping(self):
+        async with aiohttp.ClientSession() as session:
+            for painter in self.painters_names:
+                url = self.base_url.format(painter)
+                content = await self.get_requests(painter, url, session)
+                links = await self.scrap_hrefs(content)
+                self.all_painting_links.append((painter, links))
+
+            for painter, url_list in self.all_painting_links:
+                for num, url in enumerate(url_list):
+                    content = await self.get_requests(url=url, painter=painter, session=session)
+                    image_src = await self.scrap_image_href(content=content)
+                    await self.download_image(url=image_src, painter=painter, file_name=num, session=session)
+                    print("başarılı")
 
 
-    # control the director
-    @staticmethod
-    def control_directory(dir_name):
-        if os.path.exists(f"paintings/{dir_name}"):
-            pass
-        else:
-            os.mkdir(f"paintings/{dir_name}")
-            print("Path is Created")
+# Asenkron işlemi başlat
+async def start_async_scraping():
+    scrap_data = ScrapDataAsync()
+    await scrap_data.main_scraping()
 
-    def main_scraping(self):
-        num = 0
-        """all methods conceited in this method"""
-        # get all paintings
-        for painter in self.painters_names:
-            url = self.base_url.format(painter)
-            content = self.get_requests(painter=painter, url=url)
-            links = self.scrap_hrefs(content=content)
-            self.all_painting_links.append((painter, links))
-
-        # download
-        for painter, url_list in self.all_painting_links:
-            for url in url_list:
-                content = self.get_requests(url=url, painter=painter)
-                image_src = self.scrap_image_href(content=content)
-                self.download_image(url=image_src, painter=painter, file_name=num)
-                print("başarılı")
-                num += 1
-
-
-c = ScrapData()
-c.main_scraping()
-
-
-
+# Ana asenkron döngüyü çalıştır
+asyncio.run(start_async_scraping())
